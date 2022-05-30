758e7272dce78fcf025104a5d9e4b15e6a612545
Kamigami85 <yanghejie.bt04@nycu.edu.tw>
2022-05-30 20:44:20 +0800

    add css to index.php

--- db_func_test/index.php
+++ db_func_test/index.php
@@ -5,6 +5,30 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>homepage</title>
+    <style>
+        h1,h2,h3,h4{
+            text-align: center;
+        }
+        table{
+            border-collapse: collapse;
+            border:3px solid blue;
+            margin: auto;
+        }
+        table td{
+            padding:0.5rem;
+            border:1px solid #aaa;
+        }
+        table tr:nth-child(odd){
+            background: lightgreen;
+        }
+        table tr:nth-child(even){
+            background: lightcyan;
+        }
+        table tr:hover{
+            background: lightcoral;
+        }
+
+    </style>
 </head>
 <body>
     <h1>homepage</h1>
