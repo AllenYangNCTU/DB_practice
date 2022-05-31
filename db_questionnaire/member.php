8ef3c218576e6a20d0fe3d589f6c4bbaaf233f27
Kamigami85 <yanghejie.bt04@nycu.edu.tw>
2022-05-31 14:12:43 +0800

    member.php

---
+++ db_questionnaire/member.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Member center</title>
+</head>
+<body>
+<nav><a href="logout.php">Log Out</a></nav>
+    <h1>Member center</h1>
+    Log In success!
+</body>
+</html>
