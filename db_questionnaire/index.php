6b60128b63f0874a9e46f72f2b73631b484130f0
Kamigami85 <yanghejie.bt04@nycu.edu.tw>
2022-05-31 13:27:53 +0800

    create a project questionnaire and add index.php

---
+++ db_questionnaire/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Questionnaire</title>
+    <style>
+        nav{
+            width:900px;
+            margin:auto;
+            text-align: right;
+        }
+        h1{
+            text-align: center;
+        }
+    </style>
+</head>
+<body>
+    <nav>
+        <a href="login.php">Log in</a>
+    </nav>
+    <h1>Questionnaire</h1>
+</body>
+</html>
